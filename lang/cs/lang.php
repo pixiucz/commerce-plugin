518a5b6ca0b16462b7a4da3389828da216e8f1e7
Martin Chvila <mchvila@Martins-Mac.local>
2017-07-12 17:15:40 +0200

    Translation

---
+++ lang/cs/lang.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    'fields' => [
+        'active' => 'Aktivní',
+        'address' => 'Adresa',
+        'billing_address' => 'Fakturační adresa',
+        'brand' => 'Značka',
+        'categories' => 'Kategorie',
+        'city' => 'Město',
+        'color' => 'barva',
+        'country' => 'Stát',
+        'created_at' => 'Vytvořeno',
+        'decomposite_on' => 'Rozkládat podle',
+        'decreases_stock' => 'Snižuje stav zásob',
+        'delivery_address' => 'Doručovací adresa',
+        'delivery_option' => 'Doprava',
+        'first_name' => 'Jméno',
+        'images' => 'Obrázky',
+        'last_name' => 'Příjmení',
+        'name' => 'Jméno',
+        'order_status' => 'Stav objednávky',
+        'parent' => 'Předchůdce',
+        'payment_method' => 'Způsob platby',
+        'price' => 'Cena',
+        'retail_price' => 'Prodejní cena',
+        'shipping_time' => 'Doručovací doba',
+        'tax' => 'Daň',
+        'title' => 'Titulek',
+        'updated_at' => 'Upraveno',
+        'user' => 'Uživatel',
+        'visible' => 'Viditělný',
+        'zip' => 'PSČ',
+        'variants_and_attributes' => 'Varianty a jejich vlastnosti',
+        'short_description' => 'Krátký popis',
+        'long_description' => 'Dlouhý popis',
+        'rate' => 'Sazba',
+
+
+    ],
+    'comments' => [
+        'orderstatus' => [
+            'decreases_stock' => 'Ponechte neoznačený, pokud tento status znamená, že objednávka nesnižuje stav zásob.'
+        ],
+    ],
+];
