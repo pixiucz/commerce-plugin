fbaa5d3792e9442cf9890d633bcc9c7eb4193de7
Tomáš Ondruš <ondrus.tomas@outlook.com>
2017-08-30 14:36:06 +0200

    boilerplate

---
+++ components/Commerce.php
@@ -0,0 +1,19 @@
+<?php namespace Pixiu\Commerce\Components;
+
+use Cms\Classes\ComponentBase;
+
+class Commerce extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Commerce Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
