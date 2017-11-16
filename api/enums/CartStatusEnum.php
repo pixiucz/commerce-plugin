68e45efd4a9a72723957036aaf58f8f69312a6fc
martinchvila <martin.chvila@outlook.cz>
2017-11-16 13:41:59 +0100

    Revert "Temp partial testing"

--- api/enums/CartStatusEnum.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Pixiu\Commerce\api\Enums;
-
-class CartStatusEnum
-{
-    const REQUESTED_TOKEN = "requested token";
-    const ABANDONED = "abandoned";
-    const REDIRECT_SENT = "redirect sent";
-    const DONE = "done";
-}
--- models/Cart.php
+++ models/Cart.php
@@ -20,7 +20,7 @@ class Cart extends Model
     /**
      * @var array Fillable fields
      */
-    protected $fillable = ['status', 'user_id'];
+    protected $fillable = [];
 
     /**
      * @var array Relations
