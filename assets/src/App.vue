303eb0ce7be9d4e2ed7cda2ac6d85aa037bd5dee
Tomáš Ondruš <ondrus.tomas@outlook.com>
2017-12-12 16:25:06 +0100

    Element UI with TZ styles

--- assets/src/App.vue
+++ assets/src/App.vue
@@ -1,23 +1,39 @@
 <template>
   <div id="app">
+
+    <el-input />
+    <el-button type="primary">Kokoti</el-button>
+    <el-select value="fef">
+      <el-option label="Jijh" value="fef" />
+      <el-option label="Jijfgh" value="fefef" />
+      <el-option label="Jigjh" value="wufi" />
+    </el-select>
+    
+
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-};
+  import Vue from 'vue';
+  import ElementUI from 'element-ui';
+  import '../lib/element-theme/index.css';
+
+  Vue.use(ElementUI);
+
+  export default {
+    name: 'app',
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
