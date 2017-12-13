7202e0c4151de0e33b013e52d9777e32e61becc8
Tomáš Ondruš <ondrus.tomas@outlook.com>
2017-12-13 19:38:00 +0100

    Basic menu

--- assets/src/App.vue
+++ assets/src/App.vue
@@ -1,14 +1,38 @@
 <template>
   <div id="app">
-    <el-input />
-    <el-button type="primary">Kokoti</el-button>
-    <el-select value="fef">
-      <el-option label="Jijh" value="fef" />
-      <el-option label="Jijfgh" value="fefef" />
-      <el-option label="Jigjh" value="wufi" />
-    </el-select>
-    
-    <router-view/>
+    <b-navbar toggleable="md" type="dark" variant="dark">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-container>
+        <b-navbar-brand>Turistické známky</b-navbar-brand>
+
+        <b-collapse is-nav id="nav_collapse">
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item>Staré VTZ</b-nav-item>
+            <b-nav-item-dropdown text="Doplnky" right>
+              <b-dropdown-item href="#">Výročné známky</b-dropdown-item>
+              <b-dropdown-item href="#">Zrušené známky</b-dropdown-item>
+            </b-nav-item-dropdown>
+            <b-nav-item>
+              <i class="fa fa-list-alt" />
+            </b-nav-item>
+            <b-nav-item>
+              <i class="fa fa-user-circle" />
+            </b-nav-item>
+            <b-nav-item>
+              <i class="fa fa-shopping-cart" />
+            </b-nav-item>
+            <b-nav-item>
+              <el-button round size="small">Späť na web</el-button>
+            </b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+
+    <b-container>
+      <router-view/>
+    </b-container>
   </div>
 </template>
 
--- assets/src/main.js
+++ assets/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 
 import BootstrapVue from 'bootstrap-vue';
 import ElementUI from 'element-ui';
+import 'font-awesome/scss/font-awesome.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import '../lib/element-theme/index.css';
