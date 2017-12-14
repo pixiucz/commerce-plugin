1ed281aca113487032206fd7953cf4293ef188a9
Martin Chvila <martin.chvila@pixiu.cz>
2017-12-14 14:55:36 +0100

    Add getProducts method

--- assets/src/api.js
+++ assets/src/api.js
@@ -5,11 +5,19 @@ const API = '/api/v1';
 export function getCategories() {
   return new Promise((resolve, reject) => {
     Vue.http.get(`${API}/category/`).then((result) => {
-      resolve(result.body);
+      resolve(result.body.categories);
     }).catch((error) => {
       reject(error);
     });
   });
 }
 
-export function getKokot() {}
+export function getProducts(categorySlug) {
+  return new Promise((resolve, reject) => {
+    Vue.http.get(`${API}/category/${categorySlug}/products`).then((result) => {
+      resolve(result.body.products);
+    }).catch((error) => {
+      reject(error);
+    });
+  });
+}
