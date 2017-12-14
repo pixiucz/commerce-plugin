ddba163cc0abf1a5d211cd7a9bb9d6bc64b0a048
Martin Chvila <martin.chvila@pixiu.cz>
2017-12-14 20:21:24 +0100

    Link to product in product preview (no style)

--- assets/src/components/product/ProductPreview.vue
+++ assets/src/components/product/ProductPreview.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="bg-color-white hover-shadow11 padding-bottom">
-        <h3 class="font-size-23 caps font-weight5 margin-bottom1 margin-top2">
-            {{ product.product_name }}
-        </h3>
+        <router-link :to="{ name: 'detail', params: { slug: product.slug }}">
+            <h3 class="font-size-23 caps font-weight5 margin-bottom1 margin-top2">
+                {{ product.product_name }}
+            </h3>
+        </router-link>
         <p class="padding-bottom1 margin-bottom text-1"> {{ product.price }} € <i>s DPH</i></p>
         <p class="padding-bottom1 text-2">{{ product.price - (product.price * (product.tax_rate/100)) }} € <i>bez DPH</i></p>
     </div>
