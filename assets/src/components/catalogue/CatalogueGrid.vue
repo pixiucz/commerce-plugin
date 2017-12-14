e23da983133e2b4f7510d6b5887e320d84839721
Martin Chvila <martin.chvila@pixiu.cz>
2017-12-14 14:56:38 +0100

    Component init

---
+++ assets/src/components/catalogue/CatalogueGrid.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-container>
+        <b-row v-if="products">
+            <b-col cols="12" md="4" sm="2" v-for="product in products" :key="product.id">
+                <product-preview :product="product"></product-preview>
+            </b-col>
+        </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import ProductPreview from '../product/ProductPreview';
+
+export default {
+  props: ['products'],
+  components: {
+    'product-preview': ProductPreview,
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ assets/src/components/product/ProductPreview.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bg-color-white hover-shadow11 padding-bottom">
+        <h3 class="font-size-23 caps font-weight5 margin-bottom1 margin-top2">
+            {{ product.product_name }}
+        </h3>
+        <p class="padding-bottom1 margin-bottom text-1"> {{ product.price }} € <i>s DPH</i></p>
+        <p class="padding-bottom1 text-2">{{ product.price - (product.price * (product.tax_rate/100)) }} € <i>bez DPH</i></p>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['product'],
+};
+</script>
+
+<style>
+
+</style>
