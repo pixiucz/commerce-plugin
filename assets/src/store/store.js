30cbea06109831017f1b4731e659d4a129dd780c
Martin Chvila <martin.chvila@pixiu.cz>
2017-12-19 17:49:09 +0100

    Persist cart

--- assets/src/store/store.js
+++ assets/src/store/store.js
@@ -2,6 +2,7 @@
 
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 import user from './user';
 import cart from './cart';
@@ -40,4 +41,9 @@ export default new Vuex.Store({
     cart,
     products,
   },
+  plugins: [
+    createPersistedState({
+      paths: ['cart'],
+    }),
+  ],
 });
