82790705ecc3a0ad1176f060322ef3c278b517e1
Martin Chvila <martin.chvila@pixiu.cz>
2017-12-19 16:13:32 +0100

    Add home link

--- assets/src/App.vue
+++ assets/src/App.vue
@@ -6,7 +6,9 @@
         <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
         <b-container>
-          <b-navbar-brand>Turistické známky</b-navbar-brand>
+          <b-navbar-brand>
+            <router-link :to="{ name: 'home' }">Turistické známky</router-link>
+          </b-navbar-brand>
 
           <b-collapse is-nav id="nav_collapse">
             <b-navbar-nav class="ml-auto">
