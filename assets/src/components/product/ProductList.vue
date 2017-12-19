502d0e19c14dcafcda402d78427cb295cef6ee1b
Martin Chvila <martin.chvila@pixiu.cz>
2017-12-19 13:50:29 +0100

    Init

---
+++ assets/src/components/product/ProductList.vue
@@ -0,0 +1,51 @@
+<template>
+  <b-row class="product-list">
+    <b-col cols="4">
+      <b-img fluid src="http://tz.pixiu.cz/storage/app/uploads/public/595/bab/3d2/595bab3d23cec055399992.png" />
+    </b-col>
+    <b-col cols="8 to-upper mt-2">
+      <button type="button" class="close" aria-label="Close">
+        <i class="el-icon-close" @click="removeItem"></i>
+      </button>
+      <h5> {{ item.product.product_name }} </h5>
+      <h6> {{ item.product.price }} € <i class="light"> s DPH </i> </h6>
+      <h6 class="dark-grey"> {{ priceNoTax }} € bez DPH </h6>
+      <el-input-number :min="1" v-model="item.amount" @change="handleAmountChange"></el-input-number>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import { priceWithoutTax } from '@/helpers';
+
+export default {
+  props: ['item'],
+  methods: {
+    handleAmountChange(value) {
+      this.$store.dispatch('CHANGE_ITEM_AMOUNT', {
+        slug: this.item.product.slug,
+        newAmount: value,
+      });
+    },
+    removeItem() {
+      this.$store.dispatch('REMOVE_ITEM', this.item.product.slug);
+    },
+  },
+  computed: {
+    priceNoTax() {
+      return priceWithoutTax(this.item.product.price, this.item.product.tax_rate);
+    },
+  },
+};
+</script>
+
+<style>
+  .close {
+    cursor: pointer;
+  }
+
+  .product-list {
+    margin-top: 20px;
+    margin-bottom: 10px;
+  }
+</style>
--- assets/src/components/sidebar/Cart.vue
+++ assets/src/components/sidebar/Cart.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>Cart</h1>
+  <b-container>
+    <h1 class="text-center">Košík</h1>
+    <product-list 
+      v-for="item in items" 
+      :key="item.product.slug"
+      :item="item"
+    ></product-list>
+  </b-container>
 </template>
 
 <script>
+  import ProductList from '@/components/product/ProductList';
+
   export default {
     name: 'cart',
+    components: {
+      'product-list': ProductList,
+    },
+    computed: {
+      items() {
+        return this.$store.getters.getCartItems;
+      },
+    },
   };
 </script>
