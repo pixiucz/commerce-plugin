c8fbacf2c274764a5fa78b155fb816ab78935a54
Martin Chvila <martin.chvila@pixiu.cz>
2017-12-19 14:38:06 +0100

    Cleanup

--- assets/src/App.vue
+++ assets/src/App.vue
@@ -39,34 +39,6 @@
 
       <b-container>
         <router-view/>
-        <b-navbar-brand>
-          <router-link :to="{ name: 'home' }">Turistické známky</router-link>
-        </b-navbar-brand>
-
-        <b-collapse is-nav id="nav_collapse">
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item>Staré VTZ</b-nav-item>
-            <b-nav-item-dropdown text="Doplnky" right>
-              <b-dropdown-item v-for="category in categories" :key="category.id" href="#">
-                <router-link :to="{ name: 'category', params: { slug: category.slug }}">
-                  {{ category.name }}
-                </router-link>
-              </b-dropdown-item>
-            </b-nav-item-dropdown>
-            <b-nav-item>
-              <i class="fa fa-list-alt" />
-            </b-nav-item>
-            <b-nav-item>
-              <i class="fa fa-user-circle" />
-            </b-nav-item>
-            <b-nav-item>
-              <i class="fa fa-shopping-cart" />
-            </b-nav-item>
-            <b-nav-item>
-              <el-button round size="small">Späť na web</el-button>
-            </b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
       </b-container>
     </div>
 
