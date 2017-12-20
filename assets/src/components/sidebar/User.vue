e7a41d54b7b9f5f7b8dd2e914cae364be06fddde
Martin Chvila <martin.chvila@pixiu.cz>
2017-12-20 17:09:04 +0100

    Update user detail (markup only)

--- assets/src/components/sidebar/User.vue
+++ assets/src/components/sidebar/User.vue
@@ -1,13 +1,31 @@
 <template>
   <b-container class="sidebar-cart mt-3">
     <h1 class="text-center"> Uživatel <span @click="closeSidebar" class="pull-right pointer">></span></h1>
-    <div class="" v-loading="isLoading">
+    <div class="mt-3" v-loading="isLoading">
       <div v-if="!isLoggedIn">
         <el-input name="email" placeholder="Email" v-model="email"></el-input>
         <el-input name="password" type="password" placeholder="Heslo" v-model="password"></el-input>
         <el-button @click="signIn" class="sign-in-btn" type="primary">Přihlásit</el-button>
       </div>
       <div v-else>
+        <b-row>
+          <b-col cols="3" class="text-right mt-1">
+            Jmeno
+          </b-col>
+          <b-col cols="9">
+            <el-input @change="userDetailTouched = true" name="name" placeholder="Email" v-model="user.name"></el-input>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col cols="3" class="text-right mt-1">
+            Prijmeni
+          </b-col>
+          <b-col cols="9">
+            <el-input @blur="userDetailTouched = true" name="surname" placeholder="Email" v-model="user.surname"></el-input>
+          </b-col>
+        </b-row>
+        <el-button v-if="userDetailTouched" @click="updateUserDetail" class="sign-in-btn mt-2" type="primary">Uložit změny</el-button>
+        <hr>
         <el-button @click="signOut" class="sign-in-btn" type="primary">Odhlásit</el-button>
       </div>
     </div>
@@ -22,6 +40,7 @@
         email: 'test@test.cz',
         password: 'test',
         isLoading: false,
+        userDetailTouched: false,
       });
     },
     methods: {
@@ -34,6 +53,14 @@
         this.isLoading = true;
         await this.$store.dispatch('SIGN_OUT');
         this.isLoading = false;
+      },
+      updateUserDetail() {
+        this.isLoading = true;
+        setTimeout(() => {
+          this.userDetailTouched = false;
+          this.isLoading = false;
+        }, 1000);
+        this.$message('Ještě není implementováno 😱');
       },
     },
     computed: {
