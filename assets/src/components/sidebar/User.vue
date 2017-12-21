ae0ec567adc24699b8ff76735987cc6f8a5cc19a
Martin Chvila <martin.chvila@pixiu.cz>
2017-12-21 15:54:39 +0100

    Translation

--- assets/src/components/sidebar/User.vue
+++ assets/src/components/sidebar/User.vue
@@ -1,32 +1,34 @@
 <template>
   <b-container class="sidebar-cart mt-3">
-    <h1 class="text-center"> Uživatel <span @click="closeSidebar" class="pull-right pointer">></span></h1>
+    <h1 class="text-center"> {{ $t('sidebar.user.title')}} <span @click="closeSidebar" class="pull-right pointer">></span></h1>
     <div class="mt-3" v-loading="isLoading">
       <div v-if="!isLoggedIn">
         <el-input name="email" placeholder="Email" v-model="email"></el-input>
         <el-input name="password" type="password" placeholder="Heslo" v-model="password"></el-input>
-        <el-button @click="signIn" class="sign-in-btn" type="primary">Přihlásit</el-button>
+        <el-button @click="signIn" class="sign-in-btn" type="primary">{{ $t('sidebar.user.buttons.login') }}</el-button>
       </div>
       <div v-else>
         <b-row>
           <b-col cols="3" class="text-right mt-1">
-            Jmeno
+            {{ $t('sidebar.user.form.name') }}
           </b-col>
           <b-col cols="9">
-            <el-input @change="userDetailTouched = true" name="name" placeholder="Email" v-model="user.name"></el-input>
+            <el-input @change="userDetailTouched = true" name="name" v-model="user.name"></el-input>
           </b-col>
         </b-row>
         <b-row>
           <b-col cols="3" class="text-right mt-1">
-            Prijmeni
+            {{ $t('sidebar.user.form.surname') }}
           </b-col>
           <b-col cols="9">
-            <el-input @blur="userDetailTouched = true" name="surname" placeholder="Email" v-model="user.surname"></el-input>
+            <el-input @blur="userDetailTouched = true" name="surname" v-model="user.surname"></el-input>
           </b-col>
         </b-row>
-        <el-button v-if="userDetailTouched" @click="updateUserDetail" class="sign-in-btn mt-2" type="primary">Uložit změny</el-button>
+        <el-button v-if="userDetailTouched" @click="updateUserDetail" class="sign-in-btn mt-2" type="primary">
+          {{ $t('sidebar.user.buttons.saveChanges') }}
+        </el-button>
         <hr>
-        <el-button @click="signOut" class="sign-in-btn" type="primary">Odhlásit</el-button>
+        <el-button @click="signOut" class="sign-in-btn" type="primary">{{ $t('sidebar.user.buttons.logout') }}</el-button>
       </div>
     </div>
   </b-container>
@@ -60,7 +62,7 @@
           this.userDetailTouched = false;
           this.isLoading = false;
         }, 1000);
-        this.$message('Ještě není implementováno 😱');
+        this.$message('😱 😱 😱 😱 Ještě není implementováno 😱 😱 😱 😱 😱');
       },
     },
     computed: {
--- assets/src/lang/sk.js
+++ assets/src/lang/sk.js
@@ -2,4 +2,35 @@ export default {
   error: {
     api: 'Nastala chyba spojenia ({status}).',
   },
+  sidebar: {
+    orders: {
+      title: 'História objednávok',
+      noLogin: 'Pre históriu je potrebné sa prihlásiť',
+    },
+    user: {
+      title: 'Užívateľ',
+      buttons: {
+        login: 'Prihlásiť',
+        logout: 'Odhlásiť',
+        saveChanges: 'Uložiť zmeny',
+      },
+      form: {
+        name: 'Meno',
+        surname: 'Priezvisko'
+      },
+    },
+    cart: {
+      title: 'Košík',
+      misc: {
+        together: 'Spolu'
+      },
+      buttons: {
+        order: 'Objednať'
+      }
+    },
+  },
+  other: {
+    withDPH: 's DPH',
+    withoutDPH: 'bez DPH',
+  },
 };
