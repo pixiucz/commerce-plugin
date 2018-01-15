c7077e3575cb6fa140f9e1740fa219b29d209bda
Martin Chvila <martin.chvila@pixiu.cz>
2018-01-15 17:11:15 +0100

    Loadovaci animace pri prichodu na detail produktu

--- assets/src/pages/Detail.vue
+++ assets/src/pages/Detail.vue
@@ -1,49 +1,53 @@
 <template>
-  <b-container v-if="product" v-loading="isLoading">
-    <div class="category-header">
-      <p class="margin-top4 margin-bottom4 text-color">
-        <router-link v-if="categorySlug" :to="{ name: 'category', params: { slug: categorySlug }}">
-          {{ categoryName }}
-        </router-link> 
-        &gt; {{ name }}
-      </p>
+  <div>
+    <b-container v-if="product" v-loading="isLoading">
+      <div class="category-header">
+        <p class="margin-top4 margin-bottom4 text-color">
+          <router-link v-if="categorySlug" :to="{ name: 'category', params: { slug: categorySlug }}">
+            {{ categoryName }}
+          </router-link> 
+          &gt; {{ name }}
+        </p>
+      </div>
+      <b-row>
+          <b-col md="5" class="text-center">
+              <b-img v-if="product.primary_picture" fluid :src="product.primary_picture.path" />
+              <b-img v-else fluid src="http://tz.pixiu.cz/storage/app/uploads/public/595/bab/3d2/595bab3d23cec055399992.png" />
+          </b-col>
+          <b-col md="7">
+              <h1 class="big margin-top-9">{{ name }}</h1>
+              <p class="text-color margin-bottom3" v-html="product.short_description"></p>
+              <b-row>
+                  <b-col md="12" class="text-center">
+                      <div>
+                        <h4> {{ product.price | price }} {{ $t('other.withDPH')}} </h4>
+                        <p class="dark-grey"> {{ priceNoTax | price }} {{ $t('other.withoutDPH') }}</p>
+                      </div>
+                      <el-input-number v-model="amount" :min="1" :max="50"></el-input-number>             
+                      <el-button class="add-button" @click.stop="addToCart"> {{ $t('pages.detail.buttons.addToCart') }} </el-button>
+                      <p v-if="inCart > 0"> {{ $t('pages.detail.misc.inBasket') }}: <b>{{ inCart }} {{ $t('other.piece') }} </b> </p>
+                  </b-col>
+              </b-row>              
+          </b-col>
+      </b-row>
+      <b-row>
+        <b-col md="12">
+          {{ product.long_description }}
+          <br>
+          <b-table small v-if="product.specifications" striped hover :items="product.specifications">
+            <template slot="HEAD_name" slot-scope="data">
+            {{ $t('pages.detail.misc.name') }}
+            </template>
+            <template slot="HEAD_value" slot-scope="data">
+              {{ $t('pages.detail.misc.value') }}
+            </template>
+          </b-table>
+        </b-col>
+      </b-row>
+    </b-container>
+    <div style="height: 100%; width: 100%; padding-top: 45vh;" v-else v-loading="isLoading">
     </div>
-    <b-row>
-        <b-col md="5" class="text-center">
-            <b-img v-if="product.primary_picture" fluid :src="product.primary_picture.path" />
-            <b-img v-else fluid src="http://tz.pixiu.cz/storage/app/uploads/public/595/bab/3d2/595bab3d23cec055399992.png" />
-        </b-col>
-        <b-col md="7">
-            <h1 class="big margin-top-9">{{ name }}</h1>
-            <p class="text-color margin-bottom3" v-html="product.short_description"></p>
-            <b-row>
-                <b-col md="12" class="text-center">
-                    <div>
-                      <h4> {{ product.price | price }} {{ $t('other.withDPH')}} </h4>
-                      <p class="dark-grey"> {{ priceNoTax | price }} {{ $t('other.withoutDPH') }}</p>
-                    </div>
-                    <el-input-number v-model="amount" :min="1" :max="50"></el-input-number>             
-                    <el-button class="add-button" @click.stop="addToCart"> {{ $t('pages.detail.buttons.addToCart') }} </el-button>
-                    <p v-if="inCart > 0"> {{ $t('pages.detail.misc.inBasket') }}: <b>{{ inCart }} {{ $t('other.piece') }} </b> </p>
-                </b-col>
-            </b-row>              
-        </b-col>
-    </b-row>
-    <b-row>
-      <b-col md="12">
-        {{ product.long_description }}
-        <br>
-        <b-table small v-if="product.specifications" striped hover :items="product.specifications">
-          <template slot="HEAD_name" slot-scope="data">
-           {{ $t('pages.detail.misc.name') }}
-          </template>
-          <template slot="HEAD_value" slot-scope="data">
-            {{ $t('pages.detail.misc.value') }}
-          </template>
-        </b-table>
-      </b-col>
-    </b-row>
-  </b-container>
+  </div>
 </template>
 
 <script>
