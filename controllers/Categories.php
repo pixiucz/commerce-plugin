bc076c34b463fa07795e05a5fc4af1832ef272d5
Martin Chvila <martin.chvila@pixiu.cz>
2018-01-15 17:25:49 +0100

    Zobrazovani produktu v kategorii

--- controllers/Categories.php
+++ controllers/Categories.php
@@ -11,12 +11,14 @@ class Categories extends Controller
     public $implement = [
         'Backend.Behaviors.FormController',
         'Backend.Behaviors.ListController',
-        'Backend.Behaviors.ReorderController'
+        'Backend.Behaviors.ReorderController',
+        'Backend.Behaviors.RelationController',
     ];
 
     public $formConfig = 'config_form.yaml';
     public $listConfig = 'config_list.yaml';
     public $reorderConfig = 'config_reorder.yaml';
+    public $relationConfig = 'config_relation.yaml';
 
     public function __construct()
     {
