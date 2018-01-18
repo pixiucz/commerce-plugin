0979f48b578b478e6f38184c34629d7926f71f14
Martin Chvila <martin.chvila@pixiu.cz>
2018-01-18 17:51:54 +0100

    ProductImage component

---
+++ assets/src/components/product/ProductImage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <b-img v-if="product.primary_picture" fluid :src="product.primary_picture.path" />
+    <b-img v-else fluid src="http://tz.pixiu.cz/storage/app/uploads/public/595/bab/3d2/595bab3d23cec055399992.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductImage',
+  props: ['product'],
+};
+</script>
+
+<style>
+
+</style>
