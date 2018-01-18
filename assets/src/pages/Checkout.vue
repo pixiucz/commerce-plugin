b083f011570b2f96d808a5dad3be2bb20a681ab0
Martin Chvila <martin.chvila@pixiu.cz>
2018-01-18 17:52:59 +0100

    init

---
+++ assets/src/pages/Checkout.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-container>
+    <h1> Dokončení objednávky </h1>
+    <div style="height: 200px;">
+    </div>
+  </b-container>
+</template>
+
+<script>
+export default {
+  computed: {
+    items() {
+      return this.$store.getters.getCartItems;
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
