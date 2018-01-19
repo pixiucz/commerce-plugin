1f9d216b0eac11ce98405bb5e3a0780278f9ad50
Martin Chvila <martin.chvila@pixiu.cz>
2018-01-19 16:57:23 +0100

    Checkout init

--- assets/src/pages/Checkout.vue
+++ assets/src/pages/Checkout.vue
@@ -5,17 +5,30 @@
 
       <div style="padding: 10px;">
         <el-steps class="mt-3" :active="step" finish-status="success" align-center>
-          <el-step title="Shrnutí objednávky" icon="el-icon-info"></el-step>
+          <el-step title="Obsah objednávky" icon="el-icon-info"></el-step>
           <el-step title="Zákazník" icon="el-icon-location"></el-step>
           <el-step title="Způsob platby / Dopravy" icon="el-icon-menu"></el-step>
-          <el-step title="Hotovo!" icon="el-icon-star-on"></el-step>
+          <el-step title="Shrnutí objednávky" icon="el-icon-star-on"></el-step>
         </el-steps>
 
         <hr>
-        <h4 class="text-right"> Celkem: {{ overallSum }} € </h4>
-        <hr>
+        <div v-if="step === 3" class="">
+          <b-row class="text-center">
+            <b-col cols="6">
+              <h5> Objednávka bude doručena na adresu: </h5>
+              <address-card :classProp="'no-shadow no-border'" :deletable="false" :address="this.addressForm"></address-card>
+            </b-col>
+            <b-col cols="6 mt-3">
+              <h5> Způsob doručení: </h5>
+              <p> Slovenská pošta </p>
+              <h5> Způsob platby </h5>
+              <p> Dobírka</p>
+            </b-col>
+          </b-row>
+          <hr>
+        </div>
 
-        <el-table v-if="step === 0"
+        <el-table v-if="step === 0 || step === 3"
           :data="items"
           show-summary
           :sum-text="'Celkem'"
@@ -68,7 +81,7 @@
           </el-table-column>
           <el-table-column
             label="Akce"
-            width="200">
+            width="150">
             <template slot-scope="scope">
               <el-button-group>
                 <el-button size="small" type="info" icon="el-icon-minus" @click="subtractAmount(scope.row)"></el-button>
@@ -81,27 +94,68 @@
 
         <div v-if="step === 1">
           <div v-if="isLoggedIn">
-            <address-card classProp="box-card-white" v-for="address in userAddresses" :key="address.id" :address="address"></address-card>
+            <h5> Moje adresy </h5>
+            <b-row>
+              <b-col cols="4" v-for="address in userAddresses" :key="address.id" @click="useAddress(address)">
+                <address-card :deletable="false" classProp="box-card-white pointer" :address="address"></address-card>
+              </b-col>
+            </b-row>
             <hr>
           </div>
 
           <address-form ref="addressFormComponent" :address.sync="addressForm"></address-form>
+
+        </div>
+
+        <div v-if="step === 2">
+          <b-row>
+            <b-col cols="6">
+              <h4> Způsob doručení </h4>
+              <el-select style="width: 100%" v-model="selectedDeliveryOption" placeholder="Vyberte">
+                <el-option
+                  v-for="item in deliveryOptions"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+              <span v-if="!selectedDeliveryOption" style="color:red;">Povinné pole</span>
+            </b-col>
+            <b-col cols="6">
+              <h4> Způsob platby </h4>
+              <el-select style="width: 100%" v-model="selectedPaymentMethod" placeholder="Vyberte">
+                <el-option
+                  v-for="item in paymentMethods"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+              <span v-if="!selectedPaymentMethod" style="color:red;">Povinné pole</span>
+            </b-col>
+          </b-row>
         </div>
 
       </div>
+      <hr>
+      <h4 class="text-right"> Celková cena: {{ overallSum }} € </h4>
+      <hr>
     </b-container>
-    <div style="position: fixed; bottom: 0px; left: 0px; width: 100%; height: 75px;">
-      <span class="pull-right mt-3 mb-3 mr-3" style="background-color: black; padding: 10px; opacity: 0.8">
-        <el-button :disabled="step === 0" type="primary" @click="moveStep(-1)">
-          <i class="el-icon-d-arrow-left"></i>
-          Vrátit se
-        </el-button>
-        <el-button :disabled="step === 3" type="primary" @click="moveStep(1)">
-          Pokracovat
-          <i class="el-icon-d-arrow-right"></i>
-        </el-button>
-      </span>
-    </div>
+    <span class="pull-right mt-3 mb-3 mr-3">
+      <el-button v-if="step !== 0" type="primary" @click="moveStep(-1)">
+        <i class="el-icon-d-arrow-left"></i>
+        Vrátit se
+      </el-button>
+      <el-button v-if="step !== 3" type="primary" @click="moveStep(1)">
+        Pokracovat
+        <i class="el-icon-d-arrow-right"></i>
+      </el-button>
+      <el-button v-if="step === 3" type="success" @click="sendItOut">
+        <i class="el-icon-success"></i>
+        Dokončit objednávku
+      </el-button>
+    </span>
+
   </div>
 </template>
 
@@ -121,7 +175,7 @@ export default {
   },
   data() {
     return ({
-      step: 1,
+      step: 0,
       addressForm: {
         address: '',
         city: '',
@@ -133,6 +187,8 @@ export default {
         telephone: '',
         zip: '',
       },
+      selectedDeliveryOption: 1,
+      selectedPaymentMethod: 1,
     });
   },
   computed: {
@@ -152,6 +208,17 @@ export default {
     },
     userAddresses() {
       return this.$store.getters.getAddresses;
+    },
+    deliveryOptions() {
+      return [
+        { label: 'Slovenská Pošta', value: 1 },
+        { label: 'DPH', value: 2 },
+      ];
+    },
+    paymentMethods() {
+      return [
+        { label: 'Dobírka', value: 1 },
+      ];
     }
   },
   methods: {
@@ -182,6 +249,9 @@ export default {
       sums[6] = this.overallSum + ' €';
       return sums;
     },
+    useAddress(address) {
+      this.addressForm = Object.assign({}, address);
+    },
     moveStep(amount) {
       if (amount > 0) {
         if (this.step < 3 && this.canStepForward()) {
@@ -196,6 +266,10 @@ export default {
     canStepForward() {
       if (this.step === 1) {
         return this.$refs.addressFormComponent.isFormValid();
+      }
+
+      if (this.step === 2) {
+        return (this.selectedDeliveryOption && this.selectedPaymentMethod);
       }
 
       return true;
